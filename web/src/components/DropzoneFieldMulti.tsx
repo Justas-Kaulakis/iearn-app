462d8ac87572e95e4825b94611e327f29454fa40
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2021-11-29 12:59:25 +0200

    Peeked thing s in school

--- web/src/components/DropzoneFieldMulti.tsx
+++ web/src/components/DropzoneFieldMulti.tsx
@@ -52,8 +52,15 @@ const DropzoneFieldMulti: FC<
   onDeleteDBImage,
   ...props
 }) => {
-<<<<<<< HEAD
-    const [previews, setPreviews] = useState<{ id: number; prev: string }[]>([]);
+    const [previews, setPreviews] = useState<PreviewType[]>(
+      props?.imageUrls
+        ? props.imageUrls.map(({ imageUrl, id }) => ({
+          id,
+          imageUrl,
+          isFromDB: true,
+        }))
+        : []
+    );
     const settings: SliderSettings = {
       autoplay: true,
       autoplaySpeed: 5000,
@@ -64,27 +71,43 @@ const DropzoneFieldMulti: FC<
       prevArrow: <PrevArrow />,
       nextArrow: <NextArrow />,
     };
-    //console.log("Dropzone Props: ", props);
+    //console.log("Prevs", previews);
+    //console.log("Dropzone urls: ", props.imageUrls);
     return (
       <>
         <Dropzone
           accept="image/*"
           maxFiles={props?.maxFiles}
           onDrop={async (files, fileRejections) => {
-            console.log({ files, fileRejections });
+            //console.log({ files, fileRejections });
+
             if (!fileRejections.length) {
               /// compress file
-              let newPreviews: { id: number; prev: string }[] = [];
+
+              let newPreviews: Array<PreviewType> = [];
               const compressedFiles = await Promise.all(
                 files.map(async (file) => {
                   const resized = props.dim
                     ? await resizeImage(file, props.dim.x, props.dim.y)
                     : file;
+
+                  let newId = 0;
+                  let found = false;
+                  if (previews.length)
+                    while (!found) {
+                      for (let i = 0; i < previews.length; i++) {
+                        found = newId !== previews[i].id;
+                        if (!found) break;
+                      }
+                      if (!found) newId++;
+                    }
+
                   newPreviews.push({
-                    id: previews.length,
-                    prev: URL.createObjectURL(resized),
+                    id: newId,
+                    imageUrl: URL.createObjectURL(resized),
+                    isFromDB: false,
                   });
-                  return resized;
+                  return { file: resized, id: newId };
                 })
               );
               setPreviews([...previews, ...newPreviews]);
@@ -128,17 +151,14 @@ const DropzoneFieldMulti: FC<
                 {!previews ? null : (
                   <div className="dropzone-carousel">
                     <Slider {...settings}>
-                      {previews.map(({ id, prev }) => (
-                        <Fragment key={prev}>
-                          {prev || props.imageUrl ? (
+                      {previews.map(({ id, imageUrl, isFromDB }) => (
+                        <Fragment key={id}>
+                          {imageUrl ? (
                             <div
                               style={{ margin: " 5px auto 0 auto" }}
                               className="img-item"
                             >
-                              <img
-                                src={prev || props.imageUrl}
-                                alt="Uploaded File"
-                              />
+                              <img src={imageUrl} alt="Uploaded File" />
                               <IconButton
                                 rounded={0}
                                 top="0"
@@ -146,22 +166,33 @@ const DropzoneFieldMulti: FC<
                                 aria-label="close"
                                 colorScheme="red"
                                 icon={<FaTimes />}
-                                onClick={() => {
-                                  if (prev) URL.revokeObjectURL(prev);
-                                  setPreviews(
-                                    previews.filter((item) => item.id != id)
+                                onClick={async () => {
+                                  if (isFromDB) {
+                                    //console.log("Is from DB", imageUrl);
+                                    await onDeleteDBImage(id);
+                                  } else {
+                                    URL.revokeObjectURL(imageUrl);
+                                    setFieldValue(
+                                      name,
+                                      value.filter(
+                                        ({ id: fileId }) => fileId !== id
+                                      )
+                                    );
+                                  }
+                                  setPreviews((p) =>
+                                    p.filter((item) => item.id != id)
                                   );
                                 }}
                               />
-                              <Button
-                                left={0}
-                                onClick={() => {
-                                  console.log("Previews: ", previews);
-                                  console.log("Value: ", value);
-                                }}
-                              >
-                                Values
-                              </Button>
+                              {/* <Button
+                              left={0}
+                              onClick={() => {
+                                console.log("Previews: ", previews);
+                                console.log("Value: ", value);
+                              }}
+                            >
+                              Values
+                            </Button> */}
                             </div>
                           ) : null}
                         </Fragment>
@@ -176,162 +207,5 @@ const DropzoneFieldMulti: FC<
       </>
     );
   };
-=======
-  const [previews, setPreviews] = useState<PreviewType[]>(
-    props?.imageUrls
-      ? props.imageUrls.map(({ imageUrl, id }) => ({
-          id,
-          imageUrl,
-          isFromDB: true,
-        }))
-      : []
-  );
-  const settings: SliderSettings = {
-    autoplay: true,
-    autoplaySpeed: 5000,
-    speed: 1000,
-    dots: true,
-    useTransform: true,
-    cssEase: "ease-out",
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
-  };
-  //console.log("Prevs", previews);
-  //console.log("Dropzone urls: ", props.imageUrls);
-  return (
-    <>
-      <Dropzone
-        accept="image/*"
-        maxFiles={props?.maxFiles}
-        onDrop={async (files, fileRejections) => {
-          //console.log({ files, fileRejections });
-
-          if (!fileRejections.length) {
-            /// compress file
-
-            let newPreviews: Array<PreviewType> = [];
-            const compressedFiles = await Promise.all(
-              files.map(async (file) => {
-                const resized = props.dim
-                  ? await resizeImage(file, props.dim.x, props.dim.y)
-                  : file;
-
-                let newId = 0;
-                let found = false;
-                if (previews.length)
-                  while (!found) {
-                    for (let i = 0; i < previews.length; i++) {
-                      found = newId !== previews[i].id;
-                      if (!found) break;
-                    }
-                    if (!found) newId++;
-                  }
-
-                newPreviews.push({
-                  id: newId,
-                  imageUrl: URL.createObjectURL(resized),
-                  isFromDB: false,
-                });
-                return { file: resized, id: newId };
-              })
-            );
-            setPreviews([...previews, ...newPreviews]);
-            setFieldValue(
-              name,
-              Array.isArray(value)
-                ? [...value, ...compressedFiles]
-                : compressedFiles
-            );
-          } else {
-            fileRejections.forEach((fr) => {
-              let msg: string;
-              switch (fr.errors[0].code) {
-                case "file-invalid-type":
-                  msg = "Blogas failo tipas";
-                  break;
-                case "too-many-files":
-                  msg = `Failų įkėlimo max sk. ${props?.maxFiles}`;
-                  break;
-                default:
-                  msg = "Klaida, bandykite darkarta";
-                  break;
-              }
-              setFieldError(name, `'${fr.file.name}': ${msg}`);
-            });
-            console.log("Dropzone Error: ", fileRejections);
-          }
-        }}
-        {...props}
-      >
-        {({ getRootProps, getInputProps }) => (
-          <>
-            <section className="dropzone">
-              <div className="area hoverCursor" {...getRootProps()}>
-                <input {...getInputProps()} />
-                <p>Įkelti Nuotraukas</p>
-                {!value ? null : <p>{value.path}</p>}
-              </div>
-              {errors ? <span className="error">{errors[name]}</span> : null}
-
-              {!previews ? null : (
-                <div className="dropzone-carousel">
-                  <Slider {...settings}>
-                    {previews.map(({ id, imageUrl, isFromDB }) => (
-                      <Fragment key={id}>
-                        {imageUrl ? (
-                          <div
-                            style={{ margin: " 5px auto 0 auto" }}
-                            className="img-item"
-                          >
-                            <img src={imageUrl} alt="Uploaded File" />
-                            <IconButton
-                              rounded={0}
-                              top="0"
-                              size="sm"
-                              aria-label="close"
-                              colorScheme="red"
-                              icon={<FaTimes />}
-                              onClick={async () => {
-                                if (isFromDB) {
-                                  //console.log("Is from DB", imageUrl);
-                                  await onDeleteDBImage(id);
-                                } else {
-                                  URL.revokeObjectURL(imageUrl);
-                                  setFieldValue(
-                                    name,
-                                    value.filter(
-                                      ({ id: fileId }) => fileId !== id
-                                    )
-                                  );
-                                }
-                                setPreviews((p) =>
-                                  p.filter((item) => item.id != id)
-                                );
-                              }}
-                            />
-                            {/* <Button
-                              left={0}
-                              onClick={() => {
-                                console.log("Previews: ", previews);
-                                console.log("Value: ", value);
-                              }}
-                            >
-                              Values
-                            </Button> */}
-                          </div>
-                        ) : null}
-                      </Fragment>
-                    ))}
-                  </Slider>
-                </div>
-              )}
-            </section>
-          </>
-        )}
-      </Dropzone>
-    </>
-  );
-};
->>>>>>> bc4fd5898c6b3bd92fd62b77fe6c4a31ddd6dac0
 
 export default DropzoneFieldMulti;
