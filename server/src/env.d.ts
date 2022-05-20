7cebdd2e9c1c9b42cab8660fb7b03ea65326cd5c
Justas-Kaulakis <just.kaulakis@gmail.com>
2022-05-20 18:30:46 +0000

    trying to fix node_env

---
+++ server/src/env.d.ts
@@ -0,0 +1,13 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string;
+    REDIS_URL: string;
+    SERVER_PORT: string;
+    SESSION_SECRET: string;
+    CORS_ORIGIN: string;
+    DOMAIN: string;
+    EMAIL_ADRESS: string;
+    EMAIL_PASSWORD: string;
+    NODE_ENV: string;
+  }
+}
